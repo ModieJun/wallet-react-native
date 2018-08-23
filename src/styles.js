31303a470cb4250fc7c3733c940c306b8948d6f4
Junjie Lin <junjie981208@gmail.com>
2018-08-23 11:08:21 +0800

    added the styles.js  easier to maintain code

---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default  EStyleSheet.create({
+    container:{
+        flex:1,
+    },
+    text:{
+        fontSize:'20rem',
+    },
+    primaryText:{
+        fontSize:'24rem',
+        fontWeight: '300',
+    },flexRow:{
+        flexDirection:'row'
+    },ajCenter:{
+        justifyContent: 'center',
+        alignItems: 'center',
+    },greyE:{
+        backgroundColor:'#eeeeee' ,      
+    },greyD:{
+        backgroundColor:'#dddddd',
+    }
+
+});
