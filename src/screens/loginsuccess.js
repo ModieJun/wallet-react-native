86442ddd205e7f6f3ffe4e3750c78ec4d552d969
Junjie Lin <junjie981208@gmail.com>
2018-08-24 11:43:45 +0800

    adde the navigation to the next stack

--- src/screens/loginsuccess.js
+++ src/screens/loginsuccess.js
@@ -4,20 +4,40 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet,
+  StyleSheet,AsyncStorage
 } from 'react-native';
 import Lovechain from '../components/lovechainlogo';
 
 export default class LoginSuccess extends Component {
+
+  /**
+   *  AFTER THE COMPONENT MOUNTED SET TIMER TO GO TO NEXT SCREEN
+   */
+  componentDidMount(){
+    setTimeout(() => {
+      /**
+       * NEED TO NAVIGATE TO NEXT STACK OF SCREEN
+       */
+      this._signin();
+    }, 2000);
+  }
+
+  _signin= async()=> {
+    await AsyncStorage.setItem('userToken','LoginSuccess');
+    this.props.navigation.navigate('Start')
+  };
+
   render() {
+    alert('Login Success')
     return (
       <View style={styles.container}>
-        <Lovechain/>
+          <View style={{flex:1}}>
+           <Lovechain/>
+          </View>
 
-            <View style={styles.box}>
-                <Text style={styles.text}>Login Successful</Text>
-            </View>
-
+          <View style={styles.box}>
+            <Text style={styles.text}>Login Successful</Text>
+          </View>
       </View>
     );
   }
@@ -25,21 +45,23 @@ export default class LoginSuccess extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor:'#ffffff'
   },
   box:{
-    flex:1,
+    flex:8,
     justifyContent:'center',
     marginTop:20,
     marginLeft:20,
     marginRight:20,
     marginBottom:20,
     backgroundColor:'#ffffff',
-    elevation:3,
+    elevation:4,
   },
   text:{
     textAlign:'center',
     fontSize:20,
     fontWeight:'300',
+    color:'#333333'
   }
 });
